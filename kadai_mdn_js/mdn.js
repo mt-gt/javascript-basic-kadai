21295279a5c91ccfa5a9ca27aa41aac6b0be757e
Yamaguchi <59763298+mt-gt@users.noreply.github.com>
2023-12-27 07:58:21 +0900

    Update mdn.js

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,7 +1,7 @@
 const nowDate = new Date();
-
+console.log(nowDate)
 const year = nowDate.getFullYear();
-const day = nowDate.getDate();
-const month = nowDate.getMonth();
+const day = nowDate.getDate();     
+const month = nowDate.getMonth() + 1; 
 
-console.log(year+'年'+month+'月'+day+'日')
+console.log(year + '年' + month + '月' + day + '日');
