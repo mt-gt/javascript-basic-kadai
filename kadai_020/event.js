593b4300196e2a9699004a26dedfa10375ce9f44
Yamaguchi <59763298+mt-gt@users.noreply.github.com>
2023-12-27 09:10:15 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,8 @@
+// output-btnというidを持つHTML要素を取得し、定数に代入する
+const btn = document.getElementById('btn');
+
+const text = document.getElementById('text')
+
+btn.addEventListener('click', () => {
+    text.textContent ='ボタンをクリックしました';
+});
